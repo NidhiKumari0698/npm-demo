fff350ee1fc38928c492f828a7c829fd32692b2b
ni.kumari <nidhi.kumari@geminisolutions.in>
2021-07-28 10:55:23 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var _ =require('underscore');
+// let name='bulbul';
+// var mobile=7985;
+// console.log('name:',name,'mobile:',mobile);
+
+function first(){
+    let name='Nidhi';
+    var mobile=9526;
+    console.log('name:',name,'mobile:',mobile);
+
+}
+
+//first();
+
+function second(){
+    let name='chulbul';
+    var mobile=7903;
+    console.log('name:',name,'mobile:',mobile);
+}
+
+//second();
+
+// console.log('name:',name,'mobile:',mobile);
+
+let found=_.contains([1,2,3],6);
+console.log('found:',found);
